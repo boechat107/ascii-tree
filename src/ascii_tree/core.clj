5b95ea396dc0dce533ce8519c93f756c5f998454
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-01-22 14:45:33 -0200

    Some docs

--- src/ascii_tree/core.clj
+++ src/ascii_tree/core.clj
@@ -16,24 +16,27 @@
           (p0 (reverse children) nil))
       )))
 
-(defn p1
+(defn print-tree
+  "Prints a list/sequence/collection as a tree. The first element is always a root,
+  the branches or leaves are the rest of the elements."
   [tree]
   (loop [queue [tree]]
     (when (seq queue)
-      (print "\n")
+      (print "\n") ; to split levels.
       (recur 
         (reduce (fn [next-queue sub-tree] 
+                  ;; Checks if the element has children or not.
                   (if (coll? sub-tree)
                     (let [[root & children] sub-tree]
-                      (print root " ")
-                      (reduce #(conj %1 %2) 
+                      (print root " ") ; prints the root.
+                      (reduce #(conj %1 %2) ; enqueue its children for the next level.
                               next-queue
                               children))
                     (do (print sub-tree " ")
                         next-queue)))
                 [] ; next-level queue
                 queue))))
-  (print "\n")
+  (print "\n") ; just to separate the tree from the returned value.
   "Finished")
 
 ;; remove "a" before
