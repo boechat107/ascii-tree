843667611e98763fffa8c31e60f22c882f119da1
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-01-25 15:52:39 -0200

    Simplification to calc a node's width; option to set leaves spaces
    * The width of a leaf is determined by the higher level function.
    * Fn to print a leaf.
    * There are a lot of space for more simplifications, like calculating
    a subtree's width by demand instead of doing it just once. It's
    simpler but less efficient.

--- Clojure/ascii-tree/src/ascii_tree/core.clj
+++ Clojure/ascii-tree/src/ascii_tree/core.clj
@@ -5,10 +5,6 @@
   (or (string? node) (and (== 2 (count node))
                           (integer? (second node)))))
 
-(defn width
-  [elem]
-  (inc (count elem)))
-
 (defn height
   "Returns the height of a given tree."
   [node]
@@ -31,11 +27,13 @@
   "Calculates the width of a tree by calculating the width of the leaves first."
   [node]
   (if (leaf? node)
-    (list node (width node))
+    (list node (count node))
     (let [[root & branches] node
           mod-branches (map calc-width branches)
           branches-len (reduce #(+ %1 (second %2)) 0 mod-branches)
-          root-len (width root)]
+          root-len (count root)]
+      ;; If root's width is greater than branches', a fake node is inserted to print
+      ;; correctly the leaves in the future.
       (if (< root-len branches-len)
         (list* root branches-len mod-branches)
         (list* root 
@@ -44,39 +42,48 @@
                mod-branches)))))
 
 (defn print-root 
-  [root-elem w]
-  (let [n-space (/ (- w (width root-elem)) 2)]
+  [root extra-space]
+  (let [[elem w & branches] root 
+        n-space (-> (+ w (* extra-space (count branches)))
+                    (- (count elem))
+                    (/ 2))]
     (if (zero? n-space)
-      (print root-elem "")
-      (print (str (space-str (dec n-space)) root-elem (space-str (inc n-space)))))))
+      (print elem "")
+      (print (str (space-str (dec n-space)) elem (space-str (inc n-space)))))))
+
+(defn print-leaf
+  [leaf extra-space]
+  (let [[elem w] leaf]
+    (print (str elem (space-str (- (+ extra-space w) (count elem)))))))
 
 (defn print-tree
   "Prints a list/sequence/collection as a tree. The first element is always a root,
   the branches or leaves are the rest of the elements."
-  [tree]
-  (let [max-level (dec (height tree))]
-    (loop [queue [(calc-width tree)], level 0]
-      (when (seq queue)
-        (print "\n") ; to split levels.
-        (recur 
-          (reduce (fn [next-queue node] 
-                    ;; Checks if the element has children or not.
-                    (if (leaf? node)
-                      (let [[leaf w] node]
-                        (print leaf "")
-                        (if (== level max-level)
-                          next-queue
-                          (conj next-queue (make-empty-node w))))
-                      (let [[root w & branches] node]
-                        (print-root root w)
-                        (reduce #(conj %1 %2) ; enqueue its children for the next level.
-                                next-queue
-                                branches))))
-                  [] ; next-level queue
-                  queue)
-          (inc level)))))
-  (print "\n") ; just to separate the tree from the returned value.
-  "Finished")
+  ([tree] (print-tree tree 2))
+  ([tree space]
+   (let [max-level (dec (height tree))]
+     (loop [queue [(calc-width tree)], level 0]
+       (when (seq queue)
+         (print "\n") ; to split levels.
+         (recur 
+           (reduce (fn [next-queue node] 
+                     ;; Checks if the element has children or not.
+                     (if (leaf? node)
+                       (let [[leaf w] node]
+                         (print-leaf node space)
+                         (if (== level max-level)
+                           next-queue
+                           (conj next-queue (make-empty-node w))))
+                       (let [[root w & branches] node]
+                         (print-root node space)
+                         (reduce #(conj %1 %2) ; enqueue its children for the next level.
+                                 next-queue
+                                 branches))))
+                   [] ; next-level queue
+                   queue)
+           (inc level)))))
+   (print "\n") ; just to separate the tree from the returned value.
+   "Finished"))
 
 ;; remove "a" before
 ; (p0 ((b c) (d e) f) nil)
