910bf8e602b6f5860394881012c99c6f76a4fee2
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-01-26 18:43:58 -0200

    Tests for two different trees

--- Clojure/ascii-tree/test/ascii_tree/core_test.clj
+++ Clojure/ascii-tree/test/ascii_tree/core_test.clj
@@ -2,8 +2,26 @@
   (:use clojure.test
         ascii-tree.core))
 
-(defn width-test []
-  (let [t1 '("a" ("bc" "df" "gg") "h" ("e" "cc" "abc"))]
-    (= (calc-width t1)
-       '("a" 10 ("bc" 4 ("df" 2) ("gg" 2)) ("h" 1) ("e" 5 ("cc" 2) ("abc" 3))))
-    ))
+(def bigt
+  '("a" ("bc" ("df" "v" "vaca") ("gg" "123")) ("h" "helo" "ola") ("e" ("abaccaa" "w") ("abc" "ah" "fff" "hi" "aabb"))))
+
+(def smallt '("a" ("bc" "df" "gg") "h" ("e" "cc" "abc")))
+
+(deftest info-tests
+  (is (= "  _123_ " (fill-str "123" 8 \_)))
+  (is (= "123" (fill-str "123" 3 \_)))
+  (is (= " 123" (fill-str "123" 4 \_)))
+  (is (= " 123 " (fill-str "123" 5 \_)))
+  (= (add-width-info smallt 2)
+     '("a" 21 ("bc" 8 ("df" 4) ("gg" 4)) ("h" 4) ("e" 9 ("cc" 4) ("abc" 5))))
+  (is (== 3 (height smallt)))
+  (is (== 4 (height bigt))))
+
+(deftest print-tests!
+  (println bigt)
+  (print-tree! bigt)
+  (print-tree! bigt 4)
+  (newline)
+  (println smallt)
+  (print-tree! smallt)
+  (print-tree! smallt 1))
