407cf36d9ab36fdb441c344eae4a7d17adac9173
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-01-27 22:06:38 -0200

    Print test for two trees

--- Common_lisp/ascii-tree/tests.lisp
+++ Common_lisp/ascii-tree/tests.lisp
@@ -4,6 +4,9 @@
 
 (defvar smallt '("a" ("bc" "df" "gg") "h" ("e" "cc" "abc")))
 
+(defvar bigt
+  '("a" ("bc" ("df" "v" "vaca") ("gg" "123")) ("h" "helo" "ola") ("e" ("abaccaa" "w") ("abc" "ah" "fff" "hi" "aabb"))))
+
 (defun report (result form)
   (format t "~:[FAIL~;Pass~] ... ~a~%" result form))
 
@@ -19,5 +22,12 @@
     (ok? (string= " ola " (fill-str elem 5 c)))
     (ok? (string= " _ola " (fill-str elem 6 c)))
     (ok? (string= " _ola_ " (fill-str elem 7 c)))
-    (ok? (string= " __ola_ " (fill-str elem 8 c))))
-  )
+    (ok? (string= " __ola_ " (fill-str elem 8 c)))))
+
+(defun print-tests ()
+  (format t "~a~%" smallt)
+  (print-tree! smallt)
+  (print-tree! smallt 1)
+  (format t "~a~%" bigt)
+  (print-tree! bigt)
+  (print-tree! bigt 4))
